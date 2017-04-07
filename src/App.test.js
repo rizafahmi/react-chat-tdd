d077cfef693b34b0742fae2c155bdf3f8175e4a4
Riza Fahmi <rizafahmi@gmail.com>
2017-04-07 11:42:49 +0700

    Third test: chatlist initial state is []

--- src/App.test.js
+++ src/App.test.js
@@ -2,20 +2,27 @@ import React from 'react'
 import { shallow } from 'enzyme'
 
 import App from './App'
+
+let app
+
 const setup = () => {
   const wrapper = shallow(<App />)
   return wrapper
 }
+beforeEach(() => {
+  app = setup()
+})
 it('renders <ChatForm />', () => {
-  const app = setup()
   const actual = app.find('ChatForm').length
   expect(actual).toEqual(1)
 })
 it('renders <ChatList />', () => {
-  const app = setup()
   const actual = app.find('ChatList').length
   expect(actual).toEqual(1)
 })
-it('has empty array for chat list initial state')
+it('has empty array for chat list initial state', () => {
+  const actual = app.state('chatlist').length
+  expect(actual).toEqual(0)
+})
 it('has anonymous as initial state for nickname')
 it('has empty string as chat text initial state')
