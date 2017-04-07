faa13133a41e9e1f7dbe3bfedd40c72fc24807ca
Riza Fahmi <rizafahmi@gmail.com>
2017-04-07 11:44:58 +0700

    Pass test #4: 'anonymous' as initial state for nickname

--- src/App.js
+++ src/App.js
@@ -5,7 +5,8 @@ class App extends React.Component {
   constructor (props) {
     super(props)
     this.state = {
-      chatlist: []
+      chatlist: [],
+      nickname: 'anonymous'
     }
   }
   render () {
